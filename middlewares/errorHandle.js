a8de59819b4fa1f3b474308fa5612f1a00519ca8
Huy <71880560+s2huyo0ohuys2@users.noreply.github.com>
2021-09-24 22:33:54 +0700

    errorHandle and asyncHandle

---
+++ middlewares/errorHandle.js
@@ -0,0 +1,29 @@
+const ErrorRespone = require('../utils/errorHandle');
+
+const errorHandler = (err, req, res, next) => {
+    let error = {...err };
+
+    console.log(err.stack.red);
+    console.log(err.name);
+
+    if (err.name === "CastError") {
+        const message = `Resource not found with id of ${err.value}`;
+        error = new ErrorRespone(404, message);
+    }
+    if (err.code === 11000) {
+        const message = "Duplicate field value entered";
+        error = new ErrorRespone(400, message);
+    }
+    if (err.name === "ValidatorError") {
+        const message = Object.values(err.error).map(val => val.message);
+        error = new ErrorRespone(400, message);
+    }
+
+    res.status(err.statusCode || 500).json({
+        success: false,
+        error: error.message || "Server error"
+    });
+
+};
+
+module.exports = errorHandler;
