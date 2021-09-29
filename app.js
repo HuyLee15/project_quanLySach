31f3cca4c54a988cafcf0cfd2bddbc4ed0df75eb
Huy <71880560+s2huyo0ohuys2@users.noreply.github.com>
2021-09-29 22:15:57 +0700

    change port

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.use(express.json());
 
 router(app);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}!!!`));
